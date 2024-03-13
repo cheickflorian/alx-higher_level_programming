33006a755ca5936de9dd03cfcf7bb231f721d02c
cheickflorian <nganjicheickflorian@gmail.com>
2024-03-13 15:15:49 -0700

    9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        num = number % 10
+    else:
+        num = number % -10
+        num *= -1
+
+    print("{:d}".format(num), end="")
+    print(num)
